ab656fed19f390207c5f12f68a7acac52d2314d2
Ubuntu <heal@compendia.csjjpm4hwmaehhamaozmixfkca.rx.internal.cloudapp.net>
2017-02-06 13:20:44 +0000

    Admin models created

--- compendia/admin.py
+++ compendia/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import *
+
+class MyUserAdmin(admin.ModelAdmin):
+    fields = []
+
+class DoctorAdmin(admin.ModelAdmin):
+    fields = []
+
+class HospitalAdmin(admin.ModelAdmin):
+    fields = []
+
+class PrescriptionAdmin(admin.ModelAdmin):
+    fields = []
+
+class PrescribedMedicineAdmin(admin.ModelAdmin):
+    fields = []
+
+class AppointmentAdmin(admin.ModelAdmin):
+    fields = []
+
 
 # Register your models here.
+admin.site.register(MyUser, MyUserAdmin)
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Hospital, HospitalAdmin)
+admin.site.register(Prescription, PrescriptionAdmin)
+admin.site.register(PrescribedMedicine, PrescribedMedicineAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
